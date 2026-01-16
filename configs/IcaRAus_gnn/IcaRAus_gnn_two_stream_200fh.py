8fbd9b78c1020d0041d21a995fbccb5b3c51c54b
David Hunt <david.hunt@duke.edu>
2026-01-16 10:27:32 -0500

    Update optimization logs and configuration for the IcaRAus GNN two-stream model.

--- configs/IcaRAus_gnn/IcaRAus_gnn_two_stream_200fh.py
+++ configs/IcaRAus_gnn/IcaRAus_gnn_two_stream_200fh.py
@@ -2,6 +2,15 @@ import torch
 
 _base_ = ["IcaRAus_gnn_two_stream.py"]
 
+model = dict(
+    type='SequentialDynamicEdgeConv',
+    in_channels = 4,
+    hidden_channels=32,
+    out_channels=1,
+    dropout=0.5,
+    k=35
+)
+
 config_label = "IcaRAus_TwoStreamSpatioTemporalGnn_200fh"
 dataset_label = "IcaRAus_gnn_200fh"
 
@@ -10,11 +19,18 @@ generated_dataset = dict(
 )
 
 trainer = dict(
+    model = model,
     data_loader = dict(
         type='TGDataLoader',
         batch_size=40,
         shuffle=True,
         num_workers=18
+    ),
+    dataset = dict(
+        downsample_keep_ratio=0.25
+    ),
+    loss_fn = dict(
+        pos_weight = torch.tensor([0.25])
     ),
     dataset_path = generated_dataset["generated_dataset_path"],
     save_name = "{}".format(config_label),
